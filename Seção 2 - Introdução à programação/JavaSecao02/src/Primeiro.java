85ed6b037f5e452e8d70543dfd857e4de705feba
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2024-12-17 19:49:44 -0300

    Seção 2 - Seu primeiro algoritmo na prática na Linguagem Java.

---
+++ Seção 2 - Introdução à programação/JavaSecao02/src/Primeiro.java
@@ -0,0 +1,17 @@
+public class Primeiro {
+    /**
+     * <h1> O curso definitivo de Lógica de Programação. Do VisualG às principais linguagens do mercado: C, C++, Python, C# e Java </h1><br>
+     *
+     * Professor: Nelio Alves<br><br>
+     * 14. Seu primeiro algoritmo na prática<br><br>
+     * <p>
+     * <b>Note:</b> Desenvolvido na linguagem Java.
+     *
+     * @author Marcos Ferreira Shirafuchi
+     * @version 1.0
+     * @since 17/12/2024
+     */
+    public static void main(String[] args) {
+        System.out.println("Ola Mundo!");
+    }
+}
