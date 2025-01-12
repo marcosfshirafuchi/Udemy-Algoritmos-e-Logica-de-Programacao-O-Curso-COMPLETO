18a498812ff0ebd2621eac8416138a94369950f3
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-01-12 18:22:45 -0300

    147. Estrutura enquanto em C (while)

--- Seção 09 - Linguagem C/146. Estrutura condicional em C/meuprojeto/main.c
+++ Seção 09 - Linguagem C/146. Estrutura condicional em C/meuprojeto/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    int hora;
+    printf("Digite uma hora do dia: ");
+    scanf("%d", &hora);
+
+    if (hora < 12)
+    {
+        printf("Bom dia!\n");
+    }
+    else
+    {
+        printf("Boa tarde!\n");
+    }
+
     return 0;
 }
