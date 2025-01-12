84e34516863d71e41f6139a7f32083290767c444
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-01-12 14:30:58 -0300

    143. Entrada de dados em C - PARTE 2

---
+++ Seção 09 - Linguagem C/143. Entrada de dados em C - PARTE 2/programa.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+void limpar_entrada() {
+    char c;
+    while ((c = getchar()) != '\n' && c != EOF) {}
+}
+
+int main()
+{
+    int idade;
+    char nome[50];
+
+    printf("Digite o valor da idade: ");
+    scanf("%d",&idade);
+    printf("Digite o nome da pessoa: ");
+    limpar_entrada();
+    fgets(nome,50,stdin);
+
+    printf("IDADE = %d\n", idade);
+    printf("NOME = %s", nome);
+    return 0;
+}
---
+++ Seção 09 - Linguagem C/143. Entrada de dados em C - PARTE 2/programa2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+void limpar_entrada() {
+    char c;
+    while ((c = getchar()) != '\n' && c != EOF) {}
+}
+
+
+
+int main()
+{
+    int idade1, idade2;
+    char nome1[50], nome2[50];
+
+    printf("Digite o valor da idade 1: ");
+    scanf("%d",&idade1);
+    printf("Digite o nome da pessoa 1: ");
+    limpar_entrada();
+    fgets(nome1, 50, stdin);
+    strtok(nome1, "\n");
+
+    printf("Digite o valor da idade 2: ");
+    scanf("%d",&idade2);
+    printf("Digite o nome da pessoa 2: ");
+    limpar_entrada();
+    fgets(nome2, 50, stdin);
+    strtok(nome2, "\n");
+
+    printf("IDADE 1 = %d\n", idade1);
+    printf("NOME 1 = %s\n", nome1);
+    printf("IDADE 2 = %d\n", idade2);
+    printf("NOME 2 = %s\n", nome2);
+    return 0;
+}
