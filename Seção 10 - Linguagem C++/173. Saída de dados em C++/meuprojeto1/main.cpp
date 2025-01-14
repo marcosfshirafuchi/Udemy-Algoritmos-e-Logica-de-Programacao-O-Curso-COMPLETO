f603a4cb957e6a2bdfd8995d2e8a348ef2724baf
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-01-13 21:53:42 -0300

    173. Saída de dados em C++

---
+++ Seção 10 - Linguagem C++/173. Saída de dados em C++/meuprojeto1/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Bom dia";
+    cout << "Boa noite";
+    return 0;
+}
---
+++ Seção 10 - Linguagem C++/173. Saída de dados em C++/meuprojeto4/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    double x;
+    x = 2.3456;
+    cout << fixed << setprecision(2) << x << endl;
+    return 0;
+}
