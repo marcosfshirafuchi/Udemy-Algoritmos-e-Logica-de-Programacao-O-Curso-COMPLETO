d2c9894852908b88b144f9405d9a0bdecb891eeb
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-01-18 16:47:25 -0300

    214. Como executar o Debugger no Visual Studio

---
+++ Seção 12 - Linguagem C#/214. Como executar o Debugger no Visual Studio/Programa/Programa/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Programa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine(x);
+            y = x * 2;
+            Console.WriteLine(y);
+            z = int.Parse(Console.ReadLine());
+            Console.WriteLine(z);
+        }
+    }
+}
