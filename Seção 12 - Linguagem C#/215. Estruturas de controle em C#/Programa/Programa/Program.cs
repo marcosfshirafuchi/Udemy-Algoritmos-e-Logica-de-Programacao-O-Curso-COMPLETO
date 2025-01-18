1424c614faa9d6ef5b0c528578d387bfa67d12b7
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-01-18 17:02:57 -0300

    215. Estruturas de controle em C#

---
+++ Seção 12 - Linguagem C#/215. Estruturas de controle em C#/Programa/Programa/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Programa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hora;
+            Console.Write("Digite uma hora do dia: ");
+            hora = int.Parse(Console.ReadLine());
+
+            if (hora < 12) {
+                Console.WriteLine("Bom dia!");
+            }
+            else {
+                Console.WriteLine("Boa tarde!");
+            }
+        }
+    }
+}
---
+++ Seção 12 - Linguagem C#/215. Estruturas de controle em C#/Programa2/Programa2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Programa2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, soma;
+
+            soma = 0;
+            Console.Write("Digite o primeiro numero: ");
+            x = int.Parse(Console.ReadLine());
+
+            while (x != 0) {
+                soma = soma + x;
+                Console.Write("Digite outro numero: ");
+                x = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("SOMA = " + soma);
+        }
+    }
+}
