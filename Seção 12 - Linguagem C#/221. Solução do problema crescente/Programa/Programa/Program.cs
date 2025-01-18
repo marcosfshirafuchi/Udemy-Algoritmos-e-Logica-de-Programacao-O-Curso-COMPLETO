8ff4bf60ea0c0bebcf7df1fc1ac72eb58b687fc4
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-01-18 18:54:24 -0300

    221. Solução do problema crescente

---
+++ Seção 12 - Linguagem C#/221. Solução do problema crescente/Programa/Programa/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Programa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Digite dois números: ");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            while (a!=b) {
+                if (a < b) {
+                    Console.WriteLine("CRESCENTE!");
+                }
+                else {
+                    Console.WriteLine("DECRESCENTE!");
+                }
+                Console.WriteLine("Digite outros dois números: ");
+                a = int.Parse(Console.ReadLine());
+                b = int.Parse(Console.ReadLine());
+            }
+            
+        }
+    }
+}
