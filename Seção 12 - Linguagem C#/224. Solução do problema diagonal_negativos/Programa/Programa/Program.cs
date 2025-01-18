ae913b5f3c276947f50b9a7946deb31491d6fdfa
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-01-18 19:59:02 -0300

    224. Solução do problema diagonal_negativos

---
+++ Seção 12 - Linguagem C#/224. Solução do problema diagonal_negativos/Programa/Programa/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Programa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N,contNegativo;
+            
+            Console.Write("Qual a ordem da matriz? ");
+            N = int.Parse(Console.ReadLine());
+            int[,] mat = new int[N,N];
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    Console.Write("Elemento [" + i + "," + j + "]: ");
+                    mat[i,j] = int.Parse(Console.ReadLine());
+                }
+            }
+            
+            Console.WriteLine("DIAGONAL PRINCIPAL:");
+            for (int i = 0; i < N; i++) {
+                Console.Write(mat[i, i] + " ");
+            }
+            contNegativo = 0;
+            Console.WriteLine();
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (mat[i,j] < 0) {
+                        contNegativo++;
+                    }
+                }
+            }
+            Console.WriteLine("QUANTIDADE DE NEGATIVOS = " + contNegativo);
+        }
+    }
+}
