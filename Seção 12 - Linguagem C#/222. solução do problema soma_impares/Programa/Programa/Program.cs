cc9e6bcc6b779540824432851b277bdc7a89ce74
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-01-18 19:14:58 -0300

    222. solução do problema soma_impares

---
+++ Seção 12 - Linguagem C#/222. solução do problema soma_impares/Programa/Programa/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Programa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, soma = 0, troca;
+            Console.WriteLine("Digite dois números: ");
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+            if (x > y) {
+                troca = x;
+                x = y;
+                y = troca;
+            }
+            for (int i = x + 1; i < y; i++) {
+                if (i % 2 != 0) {
+                    soma = soma + i;
+                }
+            }
+            Console.WriteLine("SOMA DOS IMPARES =  " + soma);
+        }
+    }
+}
