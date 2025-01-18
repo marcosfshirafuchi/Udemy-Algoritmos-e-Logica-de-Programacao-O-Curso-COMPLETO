068de6f99f512514df94351c73877a7acdeb0788
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-01-18 19:40:15 -0300

    223. Solução do problema soma vetor

---
+++ Seção 12 - Linguagem C#/223. Solução do problema soma_vetor/Programa/Programa/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+namespace Programa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            int N;
+            double soma = 0, media;
+            Console.Write("Quantos números você vai digitar? ");
+            N = int.Parse(Console.ReadLine());
+            double[] vet = new double[N];
+            for (int i = 0; i < N; i++) {
+                Console.Write("Digite um numero: ");
+                vet[i] = double.Parse(Console.ReadLine(), CI);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write("VALORES = ");
+            for (int i = 0; i < N; i++) {
+                Console.Write(vet[i].ToString("F1", CI) + " ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < N; i++) {
+                soma = soma + vet[i];
+            }
+            Console.WriteLine("SOMA = " + soma.ToString("F2",CI));
+            media = soma / N;
+            Console.WriteLine("MEDIA = " + media.ToString("F2", CI));
+        }
+    }
+}
