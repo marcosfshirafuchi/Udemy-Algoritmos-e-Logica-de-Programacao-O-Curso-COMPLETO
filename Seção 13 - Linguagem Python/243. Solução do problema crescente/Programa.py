ece0954c49fd94b36289f8e3014bda1d383cc214
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-01-19 17:13:51 -0300

    243. Solução do problema crescente

---
+++ Seção 13 - Linguagem Python/243. Solução do problema crescente/Programa.py
@@ -0,0 +1,11 @@
+print("Digite dois números:")
+x = int(input())
+y = int(input())
+while(x != y):
+	if(x < y):
+		print("CRESCENTE!")
+	else:
+		print("DECRESCENTE!")
+	print("Digite outros dois números:")
+	x = int(input())
+	y = int(input())
