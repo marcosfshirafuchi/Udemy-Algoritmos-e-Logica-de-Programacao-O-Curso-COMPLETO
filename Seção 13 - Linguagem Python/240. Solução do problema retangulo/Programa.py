af59adc611e7d5e809649e8edde7c7366089f0de
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-01-19 16:46:39 -0300

    240. Solução do problema retangulo

---
+++ Seção 13 - Linguagem Python/240. Solução do problema retangulo/Programa.py
@@ -0,0 +1,14 @@
+import math
+
+base = float(input("Base do retângulo: "))
+altura = float(input("Altura do retângulo: "))
+
+area = base * altura
+perimetro = float = 2 * (base + altura)
+diagonal = math.sqrt(base ** 2 + altura ** 2)
+
+print(f"AREA = {area:.4f}")
+
+print(f"PERIMETO = {perimetro:.4f}")
+
+print(f"DIAGONAL = {diagonal:.4f}")
